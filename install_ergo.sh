3b99177766365ab8739314bea0e3d0ad5b8d1d86
Matthew Weger <matthew.d.weger@gmail.com>
2016-03-15 13:31:51 -0400

    adding unattended upgrades

--- install_ergo.sh
+++ install_ergo.sh
@@ -11,26 +11,30 @@
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>/var/log/ergo_install.log 2>&1
-# Everything below will go to the file 'log.out':
 
 do_start
 {
 PASSWORD="a_password"
 UNIT_ID="a_id" 
 
-
+#sets up apt sources
 mv /root/sources.list /etc/apt/sources.list
 wget -O -  http://packages.ergotelescope.org/ergo.gpg.key | apt-key add - 
 
-apt-get update
-apt-get upgrade -y
-apt-get install -y ergo-telescope 
+#sets up ergo and unattended-upgrades
+apt-get update;
+apt-get upgrade -y;
+apt-get install -y ergo-telescope unattended-upgrades apt-listchanges;
+cp /root/50unattended-upgrades /etc/apt/apt.conf.d/ ;
+apt-get update;
 
+#changes the password for the unit with the format password_unit id 
+# eg password_555
 echo -e "$PASSWORD_$UNIT_ID\n$PASSWORD_$UNIT_ID" |(passwd pi)
 echo "$UNIT_ID" > /etc/ERGO/unit_id
 
-mkdir -p /home/pi/.ssh
 #adds a key for admins. 
+mkdir -p /home/pi/.ssh
 cat /root/master_key >> /home/pi/.ssh/authorized_keys
 
 }
