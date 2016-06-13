ee0eb2a66f09dce2496bdb9a37c0c15892c72cdf
Matthew Weger <matthew.d.weger@gmail.com>
2016-06-13 16:16:27 -0400

    setting hostname

--- root_dir/usr/lib/raspi-config/install_ergo.sh
+++ root_dir/usr/lib/raspi-config/install_ergo.sh
@@ -13,10 +13,13 @@ echo "$UNIT_ID" > /etc/ERGO/unit_id
 #adds a key for local admins so i don't have to rember the unit id for every mac addr. 
 mkdir -p /home/pi/.ssh
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCy73F6b2D/BdSiWAVnTUnnhEYS1gohg1HdwKdUG30HPThfcAXPr/tcfQ6yBi4Hv1jecElyYhooSI8Tp0jI3UU1xoIl//ND8e0kzV0KPIJ3pwwzL4SEeLZ/OeXVivna19Rj0VjjpveYIIEVWMi4m6RyKT3rK6JGfnqhjVTcYu/4JiEPkWmsLUjdWani+CH2TmF1peGPvRuyjMQ0qAKdhNOBeb/RxOxJ1kUPz+UEvyiYE4EyLrz4N6YoonN+gKje8+vaeu/BgJToLYQu1qIz1mI3wws/ECOpir5ltSn/xsz4oATKKZPhcVdNnc4/BuFA86f5oVz7mUZ5faSagx+Z0Bz+eoWFY+K0puPqhDIh0xmZ3d642BXVQsagjq0bLF2tn2TbR4NB+BocYj/d4Zcj91G2yW1zNq8lCalkpaU500Oqf5AzBxtwR6RU1d49NdBv9gyrSWTCkGl8/sPyNBmldviIDKNfhXAJu1oqUq0Oi695m+pXgCARtPzlJ3d7jIHG5wmchMHwxgEAV3feFFjWye2OZiLUoFpHj1bfWnM7dYeUs/O1SMgz2OCxd2jysl+0UmffKgSjPEpHBDuPtdjGEqfIwO4eY+ePL2lmSyKudQx/0PkIHnH41qnOWWdisdcNo3tNozQpwvh6d4vO4aYwSVYpZgVfztTd4Wmrfq7WQtIopQ== 555' > /home/pi/.ssh/authorized_keys
+
 #change the hostname
 NEW_HOSTNAME="ergo$UNIT_ID"
-hostname $NEW_HOSTNAME 
-domainname $NEW_HOSTNAME
+CURRENT_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
+echo $NEW_HOSTNAME > /etc/hostname
+sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
+
 #sets up apt sources
 while ! wget -O -  http://packages.ergotelescope.org/ergo.gpg.key  ; do
         WGET_TIMER=$RANDOM
